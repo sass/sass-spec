915bb4cb6bef0d63ad60113880267edcc4c239ce
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2014-12-27 10:08:31 -0800

    use the raw output of the engine instead of the "cleaned" output with the --nuke option. Closing #210

--- lib/sass_spec/test.rb
+++ lib/sass_spec/test.rb
@@ -8,7 +8,7 @@ def run_spec_test(test_case, options = {})
   assert test_case.input_path.readable?, "Input #{test_case.input_path} file does not exist"
   assert test_case.expected_path.readable?, "Expected #{test_case.expected_path} file does not exist"
 
-  output, error, status = test_case.output
+  output, clean_output, error, status = test_case.output
 
   if status != 0 && !options[:unexpected_pass]
     msg = "Command `#{options[:sass_executable]}` did not complete:\n\n#{error}"
@@ -22,7 +22,7 @@ def run_spec_test(test_case, options = {})
   end
 
 
-  if options[:unexpected_pass] && test_case.todo? && (test_case.expected == output)
+  if options[:unexpected_pass] && test_case.todo? && (test_case.expected == clean_output)
     raise "#{test_case.input_path} passed a test we expected it to fail"
   end
 
@@ -34,7 +34,7 @@ def run_spec_test(test_case, options = {})
   end
 
   if !options[:unexpected_pass]
-    assert_equal test_case.expected, output, "Expected did not match output"
+    assert_equal test_case.expected, clean_output, "Expected did not match output"
   end
 end
 
