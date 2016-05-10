79785981509bafa73809e3acdc93682181e3c1aa
Chris Eppstein <chris@eppsteins.net>
2016-05-10 11:57:27 -0700

    Avoid cached dependency on Sass.

--- test_all_impls.sh
+++ test_all_impls.sh
@@ -8,7 +8,8 @@ do
   if [[ $GEMFILE != *"lock"* ]]
   then
     export BUNDLE_GEMFILE=$GEMFILE
-    bundle install --no-deployment || exit 1
+    # bundle install --no-deployment || exit 1
+    bundle update sass || exit 1
     bundle exec sass-spec.rb || exit 1
   fi
 done
