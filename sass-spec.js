3f8c34c643948dfa2886fe479d477ef02ac7b44b
Nick Schonning <nschonni@gmail.com>
2016-09-01 03:05:46 -0400

    Use glob and more closely match the Ruby runner
    Still has failures
    5518 passing
    604 pending
    55 failing

--- sass-spec.js
+++ sass-spec.js
@@ -7,101 +7,95 @@ var assert = require('assert'),
     sass = require('node-sass'),
     readYaml = require('read-yaml'),
     objectMerge = require('object-merge'),
+    glob = require('glob'),
     version = 3.4;
 
+var normalize = function(str) {
+  return str.replace(/\s+/g, '').replace('{', '{\n').replace(';', ';\n');
+};
+
+var specPath = join(__dirname, 'spec')
+var inputs = glob.sync(specPath + "/**/input.*");
+
+var initialize = function(folder, options) {
+  var testCase = {};
+  testCase.folder = folder;
+  testCase.name = path.basename(folder);
+  var inputScss = join(folder, 'input.scss')
+  testCase.inputPath = exists(inputScss) ? inputScss : join(folder, 'input.sass');
+  testCase.expectedPath = join(folder, 'expected_output.css');
+  testCase.errorPath = join(folder, 'error');
+  testCase.statusPath = join(folder, 'status');
+  testCase.optionsPath = join(folder, 'options.yml');
+  if (exists(testCase.optionsPath)) {
+    options = objectMerge(options, readYaml.sync(testCase.optionsPath));
+  }
+  testCase.includePaths = [
+    folder,
+    join(folder, 'sub')
+  ]
+  testCase.precision = parseFloat(options[':precision']) || 5;
+  testCase.outputStyle = options[':output_style'] ? options[':output_style'].replace(':', '') : 'nested';;
+  testCase.todo = options[':todo'] != null && options[':todo'].indexOf('libsass') != -1;
+  testCase.warningTodo = options[':warning_todo'] != null && options[':warning_todo'].indexOf('libsass') != -1;
+  testCase.startVersion = parseFloat(options[':start_version']) || 0;
+  testCase.endVersion = parseFloat(options[':end_version']) || 99;
+  testCase.options = options;
+  testCase.result = false;
+
+  // Probe filesystem once and cache the results
+  testCase.shouldFail = exists(testCase.statusPath) && !fs.statSync(testCase.statusPath).isDirectory();
+  testCase.verifyStderr = exists(testCase.errorPath) && !fs.statSync(testCase.errorPath).isDirectory();
+  return testCase;
+}
+
 describe('spec', function() {
-  var normalize = function(str) {
-    return str.replace(/\s+/g, '').replace('{', '{\n').replace(';', ';\n');
-  };
-  var suites = function() {
-    var ret = {};
-    var spec = join(__dirname, 'spec');
-    var suites = fs.readdirSync(spec);
+  inputs.forEach(function(input) {
+    var folder = path.dirname(input)
+    var options = {};
+    var optionsPath = specPath;
+    // Try and walk the directory and merge the options
+    var walkDirs = folder.replace(specPath + '/', '').split('/')
+    while (walkDirs.length > 0) {
+      optionsPath = join(optionsPath, walkDirs.shift())
+      var optionsFile = join(optionsPath, 'options.yml');
 
-    suites.forEach(function(suite) {
-      var suitePath = join(spec, suite);
-      var suiteOptions = {};
-      var suiteOptionsPath = join(suitePath, 'options.yml');
-      if (fs.existsSync(suiteOptionsPath)) {
-        suiteOptions = readYaml.sync(suiteOptionsPath);
+      if (exists(optionsFile)) {
+        options = objectMerge(options, readYaml.sync(optionsFile));
       }
+    }
 
-      var tests = fs.readdirSync(suitePath);
+    var test = initialize(folder, options);
 
-      ret[suite] = {};
-
-      tests.forEach(function(test) {
-        var testPath = join(suitePath, test);
-        var testOptions = suiteOptions;
-        var testOptionsPath = join(testPath, 'options.yml');
-        if (fs.existsSync(testOptionsPath)) {
-          testOptions = objectMerge(testOptions, readYaml.sync(testOptionsPath));
-        }
-        var hasErrorFile = fs.existsSync(join(testPath, 'error')) && !fs.statSync(join(testPath, 'error')).isDirectory();
-        var hasError = false;
-        if (hasErrorFile) {
-          var errorFileContents = fs.readFileSync(join(testPath, 'error')).toString();
-          hasError = !(
-            errorFileContents.match(/^DEPRECATION WARNING/) ||
-            errorFileContents.match(/^WARNING:/) ||
-            errorFileContents.match(/^.*?\/input.scss:\d+ DEBUG:/)
-          );
-        }
-
-        ret[suite][test] = {};
-        ret[suite][test].src = join(testPath, 'input.scss');
-        ret[suite][test].error = hasErrorFile && hasError;
-        ret[suite][test].expected = join(testPath, 'expected_output.css');
-        ret[suite][test].paths = [
-          testPath,
-          join(testPath, 'sub')
-        ];
-        ret[suite][test].options = testOptions;
-      });
-    });
-
-    return ret;
-  }();
-
-  Object.keys(suites).forEach(function(suite) {
-    var tests = Object.keys(suites[suite]);
-
-    describe(suite, function() {
-
-      tests.forEach(function(test) {
-        var t = suites[suite][test];
-        var isTodo = t.options[':todo'] != null && t.options[':todo'].indexOf('libsass') != -1;
-        var isWarningTodo = t.options[':warning_todo'] != null && t.options[':warning_todo'].indexOf('libsass') != -1;
-        var minVersion = parseFloat(t.options[':start_version']) || 0;
-        var maxVersion = parseFloat(t.options[':end_version']) || 99;
-        if (exists(t.src)) {
-          it(test, function(done) {
-            if (isTodo || isWarningTodo) {
-              this.skip("Test marked with TODO");
-            } else if (version < minVersion) {
-              this.skip("Tests marked for newer Sass versions only");
-            } else if (version > maxVersion) {
-              this.skip("Tests marked for older Sass versions only");
-            } else {
-              var expected = normalize(read(t.expected, 'utf8'));
-              sass.render({
-                file: t.src,
-                includePaths: t.paths
-              }, function(error, result) {
-                if (t.error) {
-                  assert(error);
-                } else {
-                  assert(!error);
-                }
-                if (expected) {
-                  assert.equal(normalize(result.css.toString()), expected);
-                }
-                done();
-              });
+    it(test.folder, function(done) {
+      if (test.todo || test.warningTodo) {
+        this.skip("Test marked with TODO");
+      } else if (version < test.startVersion) {
+        this.skip("Tests marked for newer Sass versions only");
+      } else if (version > test.endVersion) {
+        this.skip("Tests marked for older Sass versions only");
+      } else {
+        var expected = normalize(read(test.expectedPath, 'utf8'));
+        sass.render({
+            file: test.inputPath,
+            includePaths: test.includePaths,
+            precision: test.precision,
+            outputStyle: test.outputStyle
+          }, function(error, result) {
+            if (test.verifyStatus) {
+              assert(error);
             }
-          });
-        }
-      });
+            if (test.verifyStderr) {
+              assert(error, 'Should Error with options ' + JSON.stringify(test.options))
+            } else {
+              assert(!error, 'Shouldn\'t have an error with options ' + JSON.stringify(test.options))
+            }
+            if (expected) {
+              assert.equal(normalize(result.css.toString()), expected, 'Should equal with options ' + JSON.stringify(test.options));
+            }
+            done();
+        });
+      }
     });
   });
 });
