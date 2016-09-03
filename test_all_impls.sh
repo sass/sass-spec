5b961f8e86007a96d08a3792fb6b945b287b73b1
Nick Schonning <nschonni@gmail.com>
2016-09-03 03:56:01 -0400

    Wrap the build script so it exits Travis with a failure

--- test_all_impls.sh
+++ test_all_impls.sh
@@ -3,18 +3,19 @@ set -v
 export DIR=`dirname $0`
 
 if [ "x$NODE_SASS" == "xyes" ]; then
-  npm install
-  npm run start
-  exit 0
+  npm install || exit 1
+  npm run start || exit 1
 fi
 
-for GEMFILE in `ls $DIR/Gemfile*`
-do
-  if [[ $GEMFILE != *"lock"* ]]
-  then
-    export BUNDLE_GEMFILE=$GEMFILE
-    bundle install --no-deployment || exit 1
-    bundle update sass || exit 1
-    bundle exec sass-spec.rb || exit 1
-  fi
-done
+if [ "x$RUBY_SASS" == "xyes" ];
+  for GEMFILE in `ls $DIR/Gemfile*`
+  do
+    if [[ $GEMFILE != *"lock"* ]]
+    then
+      export BUNDLE_GEMFILE=$GEMFILE
+      bundle install --no-deployment || exit 1
+      bundle update sass || exit 1
+      bundle exec sass-spec.rb || exit 1
+    fi
+  done
+fi
