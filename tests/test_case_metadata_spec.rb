24d97f5e18b2f14f9b48ff5e7b3dde8eceaa05d5
Alex Stephen <alexstephen@google.com>
2019-08-16 16:18:53 -0700

    removing fixtures folder

--- tests/test_case_metadata_spec.rb
+++ tests/test_case_metadata_spec.rb
@@ -1,9 +1,21 @@
 require_relative 'spec_helper'
 require 'sass_spec'
 
+def create_options_yaml(dictionary)
+  Dir.mkdir('tests/fixtures') unless Dir.exist?('tests/fixtures')
+  File.write('tests/fixtures/options.yml', dictionary.to_yaml)
+end
+
+def cleanup
+  FileUtils.remove_dir('tests/fixtures')
+end
+
 describe SassSpec::TestCaseMetadata do
+  after(:each) { cleanup }
+
   context 'should ignore impl when given ignore_for' do
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/test_case_metadata/ignore_dart_sass')).ignore_for?('dart_sass') }
+    before { create_options_yaml(ignore_for: ['dart_sass']) }
+    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures')).ignore_for?('dart_sass') }
     it { is_expected.to be true }
   end
 end
