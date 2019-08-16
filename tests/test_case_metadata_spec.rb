f38de7430e00cc9d4e82238c2cc98d952979acdf
Alex Stephen <alexstephen@google.com>
2019-08-16 16:27:11 -0700

    multiple tests

--- tests/test_case_metadata_spec.rb
+++ tests/test_case_metadata_spec.rb
@@ -1,21 +1,27 @@
 require_relative 'spec_helper'
 require 'sass_spec'
 
-def create_options_yaml(dictionary)
-  Dir.mkdir('tests/fixtures') unless Dir.exist?('tests/fixtures')
-  File.write('tests/fixtures/options.yml', dictionary.to_yaml)
+def create_options_yaml(folder, dictionary)
+  FileUtils.mkdir_p("tests/fixtures/#{folder}")
+  File.write("tests/fixtures/#{folder}/options.yml", dictionary.to_yaml)
 end
 
-def cleanup
-  FileUtils.remove_dir('tests/fixtures')
+def cleanup(folder)
+  FileUtils.remove_dir("tests/fixtures/#{folder}")
 end
 
 describe SassSpec::TestCaseMetadata do
-  after(:each) { cleanup }
-
   context 'should ignore impl when given ignore_for' do
-    before { create_options_yaml(ignore_for: ['dart_sass']) }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures')).ignore_for?('dart_sass') }
+    before { create_options_yaml('ignore', ignore_for: ['dart_sass']) }
+    after { cleanup('ignore') }
+    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/ignore')).ignore_for?('dart_sass') }
     it { is_expected.to be true }
+  end
+
+  context 'should have precision' do
+    before { create_options_yaml('precision', precision: 10) }
+    after { cleanup('precision') }
+    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/precision')).precision }
+    it { is_expected.to eq 10 }
   end
 end
