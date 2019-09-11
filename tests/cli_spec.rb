7b81dc0d49c3a99ef9914a5ff72253edb7bb7478
Alex Stephen <alexstephen@google.com>
2019-09-11 15:36:03 -0700

    call properly

--- tests/cli_spec.rb
+++ tests/cli_spec.rb
@@ -2,8 +2,7 @@ require_relative 'spec_helper'
 
 RSpec.describe "run tests", type: :aruba do
   it "runs a single spec" do
-    run_command "#{Dir.pwd}/sass-spec.rb --command '#{Dir.pwd}/tests/output_hrx' --cmd-args '#{Dir.pwd}/spec/colors/basic.hrx' #{Dir.pwd}/spec/colors/basic.hrx"
-
-    expect(extract_text(last_command_started.output)).to  include("2 runs, 0 assertions, 0 failures, 0 errors, 0 skips")
+    run_command "#{Dir.pwd}/sass-spec.rb --command '#{Dir.pwd}/tests/output_hrx' --cmd-args '#{Dir.pwd}/tests/fixtures/colors/basic.hrx' #{Dir.pwd}/tests/fixtures/colors"
+    expect(last_command_started).to be_successfully_executed
   end
 end
