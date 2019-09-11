e023363d3b542c61ac63b39c5252c83f383329d1
Alex Stephen <alexstephen@google.com>
2019-09-11 15:37:59 -0700

    docs

--- tests/cli_spec.rb
+++ tests/cli_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require_relative 'spec_helper'
 
-RSpec.describe "run tests", type: :aruba do
-  it "runs a single spec" do
+RSpec.describe 'run tests', type: :aruba do
+  it 'runs a single spec' do
     run_command "#{Dir.pwd}/sass-spec.rb --command '#{Dir.pwd}/tests/output_hrx' --cmd-args '#{Dir.pwd}/tests/fixtures/colors/basic.hrx' #{Dir.pwd}/tests/fixtures/colors"
     expect(last_command_started).to be_successfully_executed
   end
