a7c4dbf59716422981f21d536efddd74899b2cc5
Alex Stephen <alexstephen@google.com>
2019-09-12 15:53:21 -0700

    PR comments

--- tests/cli_spec.rb
+++ tests/cli_spec.rb
@@ -5,8 +5,7 @@ require_relative 'spec_helper'
 RSpec.describe 'run tests', type: :aruba do
   it 'runs a single spec' do
     run_command "#{Dir.pwd}/sass-spec.rb --command "\
-                "'#{Dir.pwd}/tests/output_hrx' "\
-                "--cmd-args '#{Dir.pwd}/tests/fixtures/colors/basic.hrx' "\
+                "'#{Dir.pwd}/tests/sass_stub' "\
                 "#{Dir.pwd}/tests/fixtures/colors"
     expect(last_command_started).to be_successfully_executed
   end
