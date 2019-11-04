f60dec6aadb5dd946a1d3f6d1b645a0ac46f9e0e
Alex Stephen <alexstephen@google.com>
2019-11-04 14:41:04 -0800

    different fix

--- tests/test_case_metadata_spec.rb
+++ tests/test_case_metadata_spec.rb
@@ -4,47 +4,47 @@ require_relative 'spec_helper'
 require 'sass_spec'
 
 def create_options_yaml(folder, dictionary)
-  FileUtils.mkdir_p("tests/fixtures/#{folder}")
-  File.write("tests/fixtures/#{folder}/options.yml", dictionary.to_yaml)
+  FileUtils.mkdir_p("tests/metadata/#{folder}")
+  File.write("tests/metadata/#{folder}/options.yml", dictionary.to_yaml)
 end
 
 def cleanup(folder)
-  FileUtils.remove_dir("tests/fixtures/#{folder}")
+  FileUtils.remove_dir("tests/metadata/#{folder}")
 end
 
 describe SassSpec::TestCaseMetadata do
   context 'should ignore impl when given ignore_for' do
     before { create_options_yaml('ignore', ignore_for: ['dart_sass']) }
-    after { cleanup('metadata_ignore') }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/ignore')).ignore_for?('dart_sass') }
+    after { cleanup('ignore') }
+    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/metadata/ignore')).ignore_for?('dart_sass') }
     it { is_expected.to be true }
   end
 
   context 'should ignore impl when given only_on' do
     before { create_options_yaml('only_on', only_on: ['dart_sass']) }
-    after { cleanup('metadata_only_on') }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/only_on')).ignore_for?('libsass') }
+    after { cleanup('only_on') }
+    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/metadata/only_on')).ignore_for?('libsass') }
     it { is_expected.to be true }
   end
 
   context 'should have precision' do
     before { create_options_yaml('precision', precision: 10) }
-    after { cleanup('metadata_precision') }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/precision')).precision }
+    after { cleanup('precision') }
+    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/metadata/precision')).precision }
     it { is_expected.to eq 10 }
   end
 
   context 'should have todos for an impl' do
     before { create_options_yaml('todo', todo: ['sass/libsass#2342']) }
-    after { cleanup('metadata_todo') }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/todo')).todo?('libsass') }
+    after { cleanup('todo') }
+    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/metadata/todo')).todo?('libsass') }
     it { is_expected.to be true }
   end
 
   context 'should have warning todos for an impl' do
     before { create_options_yaml('warning', warning_todo: ['sass/libsass#2342']) }
-    after { cleanup('metadata_warning') }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/warning')).warning_todo?('libsass') }
+    after { cleanup('warning') }
+    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/metadata/warning')).warning_todo?('libsass') }
     it { is_expected.to be true }
   end
 end
