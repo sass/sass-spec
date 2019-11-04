541c99f3ad2b8ebb6fe135690a714b189619a6ef
Alex Stephen <alexstephen@google.com>
2019-11-04 14:39:45 -0800

    changing metadata test

--- tests/test_case_metadata_spec.rb
+++ tests/test_case_metadata_spec.rb
@@ -15,35 +15,35 @@ end
 describe SassSpec::TestCaseMetadata do
   context 'should ignore impl when given ignore_for' do
     before { create_options_yaml('ignore', ignore_for: ['dart_sass']) }
-    after { cleanup('ignore') }
+    after { cleanup('metadata_ignore') }
     subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/ignore')).ignore_for?('dart_sass') }
     it { is_expected.to be true }
   end
 
   context 'should ignore impl when given only_on' do
     before { create_options_yaml('only_on', only_on: ['dart_sass']) }
-    after { cleanup('only_on') }
+    after { cleanup('metadata_only_on') }
     subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/only_on')).ignore_for?('libsass') }
     it { is_expected.to be true }
   end
 
   context 'should have precision' do
     before { create_options_yaml('precision', precision: 10) }
-    after { cleanup('precision') }
+    after { cleanup('metadata_precision') }
     subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/precision')).precision }
     it { is_expected.to eq 10 }
   end
 
   context 'should have todos for an impl' do
     before { create_options_yaml('todo', todo: ['sass/libsass#2342']) }
-    after { cleanup('todo') }
+    after { cleanup('metadata_todo') }
     subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/todo')).todo?('libsass') }
     it { is_expected.to be true }
   end
 
   context 'should have warning todos for an impl' do
     before { create_options_yaml('warning', warning_todo: ['sass/libsass#2342']) }
-    after { cleanup('warning') }
+    after { cleanup('metadata_warning') }
     subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/fixtures/warning')).warning_todo?('libsass') }
     it { is_expected.to be true }
   end
