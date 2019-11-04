3ab557c7429df1b9f9d6808918b1edebd0424118
Alex Stephen <alexstephen@google.com>
2019-11-04 14:38:39 -0800

    wip

--- tests/spec_helper.rb
+++ tests/spec_helper.rb
@@ -2,3 +2,15 @@
 
 require 'rspec'
 require 'aruba/rspec'
+
+# Given the output of sass-spec,
+# return the number of tests in
+# each state (success, failed, etc)
+def test_results(output)
+  results = {}
+  matches = output.match(
+    /(?<runs>\d+) runs, (?<assertions>\d+) assertions, (?<failures>\d+) failures, (?<errors>\d+) errors, (?<skips>\d+) skips/
+  )
+  matches.names.each { |k, v| results[k.to_sym] = matches[k].to_i }
+  results
+end
