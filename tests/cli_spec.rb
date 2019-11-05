df49bf18da088d06794b908c550191928c7c9148
Alex Stephen <alexstephen@google.com>
2019-11-04 16:37:12 -0800

    helper function

--- tests/cli_spec.rb
+++ tests/cli_spec.rb
@@ -4,39 +4,34 @@ require_relative 'spec_helper'
 
 RSpec.describe 'run tests', type: :aruba do
   it 'runs a single spec' do
-    run_command "#{Dir.pwd}/sass-spec.rb --command "\
-                "'#{Dir.pwd}/tests/sass_stub' "\
-                "#{Dir.pwd}/tests/fixtures/basic"
+    run_command command('basic')
+
     expect(last_command_started).to be_successfully_executed
   end
 
   it 'should not run todo specs by default' do
-    run_command "#{Dir.pwd}/sass-spec.rb --command "\
-                "'#{Dir.pwd}/tests/sass_stub' "\
-                "#{Dir.pwd}/tests/fixtures/todo"
+    run_command command('todo')
+
     expect(last_command_started).to be_successfully_executed
     expect(test_results(last_command_started.output)[:skips]).to eq 1
   end
 
   it 'should run todo specs with --run-todo flag' do
-    run_command "#{Dir.pwd}/sass-spec.rb --run-todo --command "\
-                "'#{Dir.pwd}/tests/sass_stub' "\
-                "#{Dir.pwd}/tests/fixtures/todo"
+    run_command command('todo', ["--run-todo"])
+
     expect(last_command_started).to be_successfully_executed
     expect(test_results(last_command_started.output)[:skips]).to eq 0
   end
 
   it 'should not allow limit to take a negative number' do
-    run_command "#{Dir.pwd}/sass-spec.rb --limit -10 --command "\
-                "'#{Dir.pwd}/tests/sass_stub' "\
-                "#{Dir.pwd}/tests/fixtures/basic"
+    run_command command('basic', ["--limit -10"])
+
     expect(last_command_started).to_not be_successfully_executed
   end
 
   it 'should not allow limit to take a negative number' do
-    run_command "#{Dir.pwd}/sass-spec.rb --run-todo --limit 1 --command "\
-                "'#{Dir.pwd}/tests/sass_stub' "\
-                "#{Dir.pwd}/tests/fixtures/todo"
+    run_command command('todo', ["--run-todo", "--limit 1"])
+
     expect(last_command_started).to be_successfully_executed
     expect(test_results(last_command_started.output)[:runs]).to eq 1
   end
--- tests/spec_helper.rb
+++ tests/spec_helper.rb
@@ -14,3 +14,12 @@ def test_results(output)
   matches.names.each { |k, v| results[k.to_sym] = matches[k].to_i }
   results
 end
+
+# Gives a command string that Aruba should run for a unit test.
+# This command calls sass-spec using the sass stub.
+# It takes in the name of a fixture folder and an array of additional flags.
+def command(fixture_folder, additional_flags = [])
+  ["#{Dir.pwd}/sass-spec.rb #{additional_flags.join(' ')}",
+  "--command '#{Dir.pwd}/tests/sass_stub'",
+  "#{Dir.pwd}/tests/fixtures/#{fixture_folder}"].join(' ')
+end
