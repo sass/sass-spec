d82a85bcccf03cf36ed347e507e7bd1b3666ff48
Natalie Weizenbaum <nweiz@google.com>
2020-06-01 16:51:14 -0700

    Add a helper method for TestCaseMetadata tests

--- tests/test_case_metadata_spec.rb
+++ tests/test_case_metadata_spec.rb
@@ -12,39 +12,43 @@ def cleanup(folder)
   FileUtils.remove_dir("tests/metadata/#{folder}")
 end
 
+def metadata(folder)
+  SassSpec::TestCaseMetadata.new(SassSpec::Directory.new(folder))
+end
+
 describe SassSpec::TestCaseMetadata do
   context 'should ignore impl when given ignore_for' do
     before { create_options_yaml('ignore', ignore_for: ['dart_sass']) }
     after { cleanup('ignore') }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/metadata/ignore')).ignore_for?('dart_sass') }
+    subject { metadata('tests/metadata/ignore').ignore_for?('dart_sass') }
     it { is_expected.to be true }
   end
 
   context 'should ignore impl when given only_on' do
     before { create_options_yaml('only_on', only_on: ['dart_sass']) }
     after { cleanup('only_on') }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/metadata/only_on')).ignore_for?('libsass') }
+    subject { metadata('tests/metadata/only_on').ignore_for?('libsass') }
     it { is_expected.to be true }
   end
 
   context 'should have precision' do
     before { create_options_yaml('precision', precision: 10) }
     after { cleanup('precision') }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/metadata/precision')).precision }
+    subject { metadata('tests/metadata/precision').precision }
     it { is_expected.to eq 10 }
   end
 
   context 'should have todos for an impl' do
     before { create_options_yaml('todo', todo: ['sass/libsass#2342']) }
     after { cleanup('todo') }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/metadata/todo')).todo?('libsass') }
+    subject { metadata('tests/metadata/todo').todo?('libsass') }
     it { is_expected.to be true }
   end
 
   context 'should have warning todos for an impl' do
     before { create_options_yaml('warning', warning_todo: ['sass/libsass#2342']) }
     after { cleanup('warning') }
-    subject { SassSpec::TestCaseMetadata.new(SassSpec::Directory.new('tests/metadata/warning')).warning_todo?('libsass') }
+    subject { metadata('tests/metadata/warning').warning_todo?('libsass') }
     it { is_expected.to be true }
   end
 end
