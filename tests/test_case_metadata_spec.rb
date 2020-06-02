66903b40e8d21af4408aaa978dc260fec7233e08
Natalie Weizenbaum <nweiz@google.com>
2020-06-01 17:40:33 -0700

    Flesh out TestCaseMetadata specs

--- tests/test_case_metadata_spec.rb
+++ tests/test_case_metadata_spec.rb
@@ -5,6 +5,7 @@ require 'sass_spec'
 
 describe SassSpec::TestCaseMetadata do
   include_context :uses_temp_dir
+  after(:each) { SassSpec::TestCaseMetadata.cache.clear }
 
   def create_options_yaml(folder=nil, dictionary)
     dir = folder ? File.join(self.dir, folder) : self.dir
@@ -17,13 +18,58 @@ describe SassSpec::TestCaseMetadata do
       SassSpec::Directory.new(folder ? File.join(self.dir, folder) : self.dir))
   end
 
+  describe '#initialize' do
+    it 'should load all options from the directory' do
+      create_options_yaml(foo: 'bar', baz: 'qux', zip: 'zap')
+      expect(metadata.options).to be == {foo: 'bar', baz: 'qux', zip: 'zap'}
+    end
+
+    context 'with a parent directory' do
+      it 'should include options from the parent and child' do
+        create_options_yaml(foo: 'bar')
+        create_options_yaml('child', baz: 'qux')
+        create_options_yaml('child/grandchild', zip: 'zap')
+        expect(metadata('child/grandchild').options).to be == {foo: 'bar', baz: 'qux', zip: 'zap'}
+      end
+
+      it 'should prefer child options to parent options' do
+        create_options_yaml(foo: 'parent')
+        create_options_yaml('child', foo: 'child')
+        expect(metadata('child').options).to be == {foo: 'child'}
+      end
+
+      it 'should consider directories without options to have empty options' do
+        create_options_yaml(foo: 'bar')
+        create_options_yaml('child/grandchild', baz: 'qux')
+        expect(metadata('child/grandchild').options).to be == {foo: 'bar', baz: 'qux'}
+      end
+
+      context 'and mergeable options' do
+        it 'should add each value to a list' do
+          create_options_yaml(ignore_for: 'dart-sass')
+          create_options_yaml('child', ignore_for: 'libsass')
+          create_options_yaml('child/grandchild', ignore_for: 'future-sass')
+          expect(metadata('child/grandchild').options).to(
+            be == {ignore_for: %w[dart-sass libsass future-sass]})
+        end
+
+        it 'should concatenate list values' do
+          create_options_yaml(ignore_for: 'dart-sass')
+          create_options_yaml('child', ignore_for: ['libsass', 'future-sass'])
+          expect(metadata('child').options).to(
+            be == {ignore_for: %w[dart-sass libsass future-sass]})
+        end
+      end
+    end
+  end
+
   it 'should ignore impl when given ignore_for' do
-    create_options_yaml(ignore_for: ['dart_sass'])
-    expect(metadata.ignore_for?('dart_sass')).to be true
+    create_options_yaml(ignore_for: ['dart-sass'])
+    expect(metadata.ignore_for?('dart-sass')).to be true
   end
 
   it 'should ignore impl when given only_on' do
-    create_options_yaml(only_on: ['dart_sass'])
+    create_options_yaml(only_on: ['dart-sass'])
     expect(metadata.ignore_for?('libsass')).to be true
   end
 
@@ -32,13 +78,25 @@ describe SassSpec::TestCaseMetadata do
     expect(metadata.precision).to eq 10
   end
 
-  it 'should have todos for an impl' do
-    create_options_yaml(todo: ['sass/libsass#2342'])
-    expect(metadata.todo?('libsass')).to be true
-  end
+  context 'with todos' do
+    it 'should load todos from implementation names' do
+      create_options_yaml(todo: ['libsass'])
+      expect(metadata.todo?('libsass')).to be true
+    end
 
-  it 'should have warning todos for an impl' do
-    create_options_yaml(warning_todo: ['sass/libsass#2342'])
-    expect(metadata.warning_todo?('libsass')).to be true
+    it 'should load warning_todos from implementation names' do
+      create_options_yaml(warning_todo: ['libsass'])
+      expect(metadata.warning_todo?('libsass')).to be true
+    end
+
+    it 'should load todos from issue numbers' do
+      create_options_yaml(todo: ['sass/libsass#2342'])
+      expect(metadata.todo?('libsass')).to be true
+    end
+
+    it 'should load warning_todos from issue numbers' do
+      create_options_yaml(warning_todo: ['sass/libsass#2342'])
+      expect(metadata.warning_todo?('libsass')).to be true
+    end
   end
 end
