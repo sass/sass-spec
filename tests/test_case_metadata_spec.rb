14dc4bdc71379059ca5f0463e47bae02f0cea18b
Natalie Weizenbaum <nweiz@google.com>
2020-06-01 17:22:36 -0700

    Factor out some boilerplate

--- tests/test_case_metadata_spec.rb
+++ tests/test_case_metadata_spec.rb
@@ -3,52 +3,42 @@
 require_relative 'spec_helper'
 require 'sass_spec'
 
-def create_options_yaml(folder, dictionary)
-  FileUtils.mkdir_p("tests/metadata/#{folder}")
-  File.write("tests/metadata/#{folder}/options.yml", dictionary.to_yaml)
-end
-
-def cleanup(folder)
-  FileUtils.remove_dir("tests/metadata/#{folder}")
-end
-
-def metadata(folder)
-  SassSpec::TestCaseMetadata.new(SassSpec::Directory.new(folder))
-end
-
 describe SassSpec::TestCaseMetadata do
-  context 'should ignore impl when given ignore_for' do
-    before { create_options_yaml('ignore', ignore_for: ['dart_sass']) }
-    after { cleanup('ignore') }
-    subject { metadata('tests/metadata/ignore').ignore_for?('dart_sass') }
-    it { is_expected.to be true }
+  include_context :uses_temp_dir
+
+  def create_options_yaml(folder=nil, dictionary)
+    dir = folder ? File.join(self.dir, folder) : self.dir
+    FileUtils.mkdir_p(dir)
+    File.write("#{dir}/options.yml", dictionary.to_yaml)
   end
 
-  context 'should ignore impl when given only_on' do
-    before { create_options_yaml('only_on', only_on: ['dart_sass']) }
-    after { cleanup('only_on') }
-    subject { metadata('tests/metadata/only_on').ignore_for?('libsass') }
-    it { is_expected.to be true }
+  def metadata(folder=nil)
+    SassSpec::TestCaseMetadata.new(
+      SassSpec::Directory.new(folder ? File.join(self.dir, folder) : self.dir))
   end
 
-  context 'should have precision' do
-    before { create_options_yaml('precision', precision: 10) }
-    after { cleanup('precision') }
-    subject { metadata('tests/metadata/precision').precision }
-    it { is_expected.to eq 10 }
+  it 'should ignore impl when given ignore_for' do
+    create_options_yaml(ignore_for: ['dart_sass'])
+    expect(metadata.ignore_for?('dart_sass')).to be true
   end
 
-  context 'should have todos for an impl' do
-    before { create_options_yaml('todo', todo: ['sass/libsass#2342']) }
-    after { cleanup('todo') }
-    subject { metadata('tests/metadata/todo').todo?('libsass') }
-    it { is_expected.to be true }
+  it 'should ignore impl when given only_on' do
+    create_options_yaml(only_on: ['dart_sass'])
+    expect(metadata.ignore_for?('libsass')).to be true
   end
 
-  context 'should have warning todos for an impl' do
-    before { create_options_yaml('warning', warning_todo: ['sass/libsass#2342']) }
-    after { cleanup('warning') }
-    subject { metadata('tests/metadata/warning').warning_todo?('libsass') }
-    it { is_expected.to be true }
+  it 'should have precision' do
+    create_options_yaml(precision: 10)
+    expect(metadata.precision).to eq 10
+  end
+
+  it 'should have todos for an impl' do
+    create_options_yaml(todo: ['sass/libsass#2342'])
+    expect(metadata.todo?('libsass')).to be true
+  end
+
+  it 'should have warning todos for an impl' do
+    create_options_yaml(warning_todo: ['sass/libsass#2342'])
+    expect(metadata.warning_todo?('libsass')).to be true
   end
 end
