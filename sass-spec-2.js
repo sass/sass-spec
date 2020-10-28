e78be78176603bfb0e6476b0e794de713cbf4830
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-10-27 17:52:04 -0700

    works

--- sass-spec-2.js
+++ sass-spec-2.js
@@ -1,7 +1,10 @@
+const tap = require("tap")
 const { promisify } = require("util")
 const fs = require("fs")
 const path = require("path")
 const yaml = require("js-yaml")
+const child_process = require("child_process")
+const { assert } = require("console")
 
 const readdir = promisify(fs.readdir)
 const stat = promisify(fs.stat)
@@ -54,11 +57,82 @@ async function iterateDir(dir, opts, cb) {
       // FIXME handle .hrx
     } else if (filename === "input.scss" || filename === "input.sass") {
       // If this directory contains an input file, then run the test on it
-      await cb(dir)
+      await cb(dir, opts)
     }
   }
 }
 
-iterateDir("spec", { impl: "dart-sass" }, (dir) => {
-  console.log(dir)
-})
+const impl = "dart-sass"
+
+/**
+ * Return whether the file should have a successful output
+ */
+function hasOutputFile(files, impl) {
+  return (
+    files.includes(`output-${impl}.css`) ||
+    (files.includes("output.css") && !files.includes(`error-${impl}`))
+  )
+}
+
+const bins = {
+  "dart-sass": "sass --load-path=spec --no-unicode",
+  libsass: `${path.resolve(
+    process.cwd(),
+    "../libsass/sassc/bin/sassc"
+  )} --style expanded --load-path=spec`,
+}
+
+function normalizeOutput(output = "") {
+  return output.replace(/\r?\n+/g, "\n").trim()
+}
+
+function escape(text) {
+  return text.replace(/\n/g, "\\n").replace(/\r/g, "\\r")
+}
+
+/**
+ * Run a sass spec test on the given directory with the given options
+ */
+async function runTest(dir, opts) {
+  const { impl } = opts
+  const files = await readdir(dir)
+  // determine whether the syntax is indented or not
+  const indented = files.includes("input.sass")
+  const inputFile = indented ? "input.sass" : "input.scss"
+
+  const bin = bins[impl]
+  // TODO does this work when the cwd is the current directory?
+  const cmdOpts = [`--load-path=spec`]
+  // Pass in the indentend option to the command
+  if (indented) {
+    cmdOpts.push(impl === "dart-sass" ? "--indented" : "--sass")
+  }
+  cmdOpts.push(inputFile)
+  const cmd = `${bin} ${cmdOpts.join(" ")}`
+
+  // determine whether this test has a valid output or an error
+  const isSuccessCase = hasOutputFile(files, impl)
+
+  process.chdir(dir)
+  tap.test(dir, async (t) => {
+    if (isSuccessCase) {
+      // valid case
+      const outputFilename = files.includes(`output-${impl}.css`)
+        ? `output-${impl}.css`
+        : "output.css"
+      const expected = await readFile(path.resolve(dir, outputFilename), {
+        encoding: "utf-8",
+      })
+      const actual = child_process.execSync(cmd, { encoding: "utf-8" })
+      // console.log(escape(expected))
+      // console.log(escape(actual))
+      t.equal(normalizeOutput(actual), normalizeOutput(expected), dir)
+    } else {
+      // error case
+    }
+  })
+
+  // run the implementation
+}
+
+iterateDir("spec", { impl }, runTest)
