833c6152642c3098a1ee58d37c347ee7a683a9a4
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-11-11 16:20:48 -0800

    actual.test.ts passes again

--- test/actual.test.ts
+++ test/actual.test.ts
@@ -1,56 +1,55 @@
 import tap from "tap"
 import path from "path"
 import { getActualResult } from "../lib-js/execute"
-import { withArchive } from "../lib-js/hrx"
+import { fromPath } from "../lib-js/spec-path"
 
-withArchive(
-  path.resolve(__dirname, "./fixtures/actual.hrx"),
-  async (baseDir) => {
-    await tap.test("getActualResult", async (t) => {
-      async function getResults(subdir: string) {
-        return getActualResult(path.resolve(baseDir, subdir), {
-          rootDir: "",
-          impl: "sass-mock",
-          bin:
-            "node " + path.resolve(__dirname, "./fixtures/sass-exec-mock.js"),
-        })
-      }
-      t.todo("does not print to parent stderr")
-      t.hasStrict(
-        await getResults("output"),
-        {
-          output: "OUTPUT",
-          isSuccess: true,
-        },
-        "populates only `output` on successful execution"
-      )
-
-      t.hasStrict(
-        await getResults("error"),
-        {
-          error: "ERROR",
-          isSuccess: false,
-        },
-        "populates only `error` on execution failure"
-      )
-      t.hasStrict(
-        await getResults("warning"),
-        {
-          output: "OUTPUT",
-          warning: "WARNING",
-          isSuccess: true,
-        },
-        "populates both `output` and `warning` on execussion success with stderr content"
-      )
-
-      t.test("options", (t) => {
-        t.todo("passes rootDir correctly")
-        t.todo("passes precision argument correctly")
-        t.todo("passes indented argument correctly")
-        t.todo("passes indented argument correctly for dart-sass/libsass")
-        t.end()
+tap.test("getActualResult", async (t) => {
+  const dir = await fromPath(path.resolve(__dirname, "./fixtures/actual.hrx"))
+  await dir.withRealFiles(async () => {
+    async function getResults(subpath: string) {
+      const subdir = await dir.atPath(subpath)
+      return getActualResult(subdir, {
+        rootDir: "",
+        impl: "sass-mock",
+        bin: "node",
+        cmdOpts: [path.resolve(__dirname, "./fixtures/sass-exec-mock.js")],
       })
+    }
+    t.todo("does not print to parent stderr")
+    t.hasStrict(
+      await getResults("output"),
+      {
+        output: "OUTPUT",
+        isSuccess: true,
+      },
+      "populates only `output` on successful execution"
+    )
+
+    t.hasStrict(
+      await getResults("error"),
+      {
+        error: "ERROR",
+        isSuccess: false,
+      },
+      "populates only `error` on execution failure"
+    )
+    t.hasStrict(
+      await getResults("warning"),
+      {
+        output: "OUTPUT",
+        warning: "WARNING",
+        isSuccess: true,
+      },
+      "populates both `output` and `warning` on execussion success with stderr content"
+    )
+
+    t.test("options", (t) => {
+      t.todo("passes rootDir correctly")
+      t.todo("passes precision argument correctly")
+      t.todo("passes indented argument correctly")
+      t.todo("passes indented argument correctly for dart-sass/libsass")
       t.end()
     })
-  }
-)
+    t.end()
+  })
+})
