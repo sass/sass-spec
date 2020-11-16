1df3b748e49c1c2ab125e0d36bc7d2736f6d3989
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-11-15 17:47:07 -0800

    clean up interaction loop

--- lib-js/interactive.ts
+++ lib-js/interactive.ts
@@ -1,17 +1,60 @@
 import readline from "readline"
 import { SpecPath } from "./spec-path"
-import { FailTestResult } from "./test-case"
+import { TestResult, FailTestResult } from "./test-case"
 
-// TODO more options
-const options = {
-  t: "Show me the test case.",
-  // d: "Show diff",
+interface InteractorOption {
+  key: string
+  description: string
+  /**
+   * The predicate to fulfill in order to display this command option.
+   * If this is not defined, then this option is always shown.
+   */
+  requirement?(dir: SpecPath, result: FailTestResult): boolean
+  /**
+   * The function to call to resolve this option.
+   * If this function returns a value, the interactive mode should quit with that value,
+   * otherwise continue.
+   */
+  resolve(dir: SpecPath, result: FailTestResult): Promise<TestResult | void>
+}
+
+const options: InteractorOption[] = [
+  {
+    key: "t",
+    description: "Show me the test case.",
+    async resolve(dir) {
+      console.log(await dir.input())
+    },
+  },
+  {
+    key: "d",
+    description: "Show diff.",
+    requirement(dir, result) {
+      return !!result.error.diff
+    },
+    async resolve(dir, result) {
+      console.log(result.error.diff)
+    },
+  },
   // O: "Update expected output and pass test",
+  // T: "Mark spec as todo for [impl]"
   // I: "Migrate copy of test to pass on [impl]",
   // G: "Ignore test for [impl] FOREVER",
-  f: "Mark as failed",
-  X: "Exit testing",
-}
+  {
+    key: "f",
+    description: "Mark as failed.",
+    async resolve(_, result) {
+      return result
+    },
+  },
+  {
+    key: "X",
+    description: "Exit testing.",
+    async resolve() {
+      process.exit(0)
+    },
+  },
+]
 
 export async function interactiveMode(dir: SpecPath, result: FailTestResult) {
   const rl = readline.createInterface({
@@ -32,31 +75,24 @@ export async function interactiveMode(dir: SpecPath, result: FailTestResult) {
     console.log(result.error.message)
 
     // TODO
-    for (const [option, description] of Object.entries(options)) {
-      console.log(`${option}. ${description}`)
+    const validOptions = options.filter(
+      ({ requirement }) => !requirement || requirement(dir, result)
+    )
+    for (const { key, description } of validOptions) {
+      console.log(`${key}. ${description}`)
     }
     // TODO show prompts
-    const [option, repeat] = await question("Please select an option > ")
-    switch (option) {
-      // Show me the test case
-      case "t": {
-        console.log(await dir.input())
-        break
-      }
-      // Mark as failed
-      case "f": {
-        // FIXME are there cases where you the thing changes to success?
-        setTimeout(() => rl.close())
-        return result
-      }
-      // Exit testing
-      case "X": {
-        setTimeout(() => rl.close())
-        process.exit(0)
-      }
-      default: {
-        console.log(`Invalid option chosen: ${option}`)
-      }
+    const [key, repeat] = await question("Please select an option > ")
+    const chosen = validOptions.find((o) => o.key === key)
+    if (!chosen) {
+      console.log(`Invalid option chosen: ${key}`)
+      continue
+    }
+    const res = await chosen.resolve(dir, result)
+    // If the resolve returns an argument, close the interaction loop and return it
+    if (res) {
+      rl.close()
+      return result
     }
   }
 }
