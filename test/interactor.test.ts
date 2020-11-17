03f22cefa24f7bcd8764b5b932db9da6d554472b
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-11-16 16:26:16 -0800

    add interactor tests

---
+++ test/interactor.test.ts
@@ -0,0 +1,16 @@
+import { Interactor } from "../lib-js/interactor"
+import { Readable, Writable } from "stream"
+
+describe("Interactor", () => {
+  it.todo("displays an input prompt with options")
+
+  it.todo("displays the right options for each failure type")
+
+  it.todo("displays options again if a print option is chosen")
+
+  it.todo("exits the loop with the answer if a non-thing statement is chosen")
+
+  it.todo("keeps track of chosen options")
+
+  it.todo("handles each option correctly")
+})
