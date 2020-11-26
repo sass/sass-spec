7399094f7b56759ca7c7de4cdeb08255bef67993
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-11-25 16:22:18 -0800

    move functions inside

--- lib-js/compiler.ts
+++ lib-js/compiler.ts
@@ -46,15 +46,46 @@ export function executableCompiler(
   }
 }
 
-/**
- * Create a child process that uses the dart-sass repo at the path,
- * and compiles the files input to stdin.
- */
-async function createDartProcess(repoPath: string) {
-  if (!fs.existsSync(path.resolve(repoPath, "bin/sass.dart"))) {
-    throw new Error(`${repoPath} is not a valid Dart Sass repository`)
+export class DartCompiler implements Compiler {
+  private readonly stdin: Writable
+  private readonly stdout: AsyncGenerator<string>
+  private readonly stderr: AsyncGenerator<string>
+
+  private constructor(
+    dart: ChildProcessWithoutNullStreams,
+    private readonly initArgs: string[] = []
+  ) {
+    this.stdin = dart.stdin
+    this.stdout = DartCompiler.toChunks(dart.stdout)
+    this.stderr = DartCompiler.toChunks(dart.stderr)
+    this.initArgs = initArgs
   }
-  const dartFile = `
+
+  /**
+   * Create a dart-sass compiler from the repo given by the path.
+   */
+  static async fromRepo(path: string, initArgs: string[] = []) {
+    return new DartCompiler(await this.createProcess(path), initArgs)
+  }
+
+  async compile(path: string, opts: string[]) {
+    this.stdin.write(`!cd ${path}\n`)
+    this.stdin.write([...this.initArgs, ...opts].join(" ") + "\n")
+    return {
+      stdout: (await this.stdout.next()).value,
+      stderr: (await this.stderr.next()).value,
+      status: +(await this.stdout.next()).value,
+    }
+  }
+  /**
+   * Create a child process that uses the dart-sass repo at the path,
+   * and compiles the files piped to stdin.
+   */
+  private static async createProcess(repoPath: string) {
+    if (!fs.existsSync(path.resolve(repoPath, "bin/sass.dart"))) {
+      throw new Error(`${repoPath} is not a valid Dart Sass repository`)
+    }
+    const dartFile = `
 // @dart=2.9
 import "dart:convert";
 import "dart:io";
@@ -84,95 +115,63 @@ main() async {
     exitCode = 0;
   }
 }`
-  const dartFilename = path.resolve(os.tmpdir(), "sass-wrapper.dart")
-  await fs.promises.writeFile(dartFilename, dartFile, { encoding: "utf-8" })
-  const child = child_process.spawn("dart", [
-    "--enable-asserts",
-    `--packages=${repoPath}/.packages`,
-    dartFilename,
-  ])
-  // TODO figure out how to delete the file without crashing the process
-  // fs.unlinkSync(dartFilename)
-  // child.on("spawn", () => {
-  //   fs.unlinkSync(dartFilename)
-  // })
-  return child
-}
-
-function splitSingle(buffer: Buffer, token: number) {
-  const idx = buffer.indexOf(token)
-  if (idx === -1) {
-    return [buffer]
-  } else {
-    return [buffer.slice(0, idx), buffer.slice(idx + 1)]
+    const dartFilename = path.resolve(os.tmpdir(), "sass-wrapper.dart")
+    await fs.promises.writeFile(dartFilename, dartFile, { encoding: "utf-8" })
+    const child = child_process.spawn("dart", [
+      "--enable-asserts",
+      `--packages=${repoPath}/.packages`,
+      dartFilename,
+    ])
+    // TODO figure out how to delete the file without crashing the process
+    // fs.unlinkSync(dartFilename)
+    // child.on("spawn", () => {
+    //   fs.unlinkSync(dartFilename)
+    // })
+    return child
   }
-}
 
-// Split a buffer using the given token
-function split(buffer: Buffer, token: number) {
-  const segments = []
-  let [head, tail] = splitSingle(buffer, token)
-  while (tail) {
-    segments.push(head)
-    const [head2, tail2] = splitSingle(tail, token)
-    head = head2
-    tail = tail2
-  }
-  segments.push(head)
-  return segments
-}
-
-// Split the stream into chunks based on the break character (0xff)
-// TODO need to test this
-async function* toDartChunks(stream: Readable) {
-  let buff = Buffer.from("")
-  for await (const chunk of stream) {
-    const [head, ...tail] = split(chunk, 0xff)
-    // If we received *any* break characters, yield those segments
-    if (tail.length > 0) {
-      yield Buffer.concat([buff, head]).toString()
-      for (const item of tail.slice(0, tail.length - 1)) {
-        yield item.toString()
-      }
-      // Set the buffer to the last unfinished segment
-      buff = tail[tail.length - 1]
+  private static splitSingle(buffer: Buffer, token: number) {
+    const idx = buffer.indexOf(token)
+    if (idx === -1) {
+      return [buffer]
     } else {
-      // If we didn't receive any 0xff in this chunk, just append to the
-      // intermediate buffer
-      buff = Buffer.concat([buff, head])
+      return [buffer.slice(0, idx), buffer.slice(idx + 1)]
     }
   }
-}
 
-export class DartCompiler implements Compiler {
-  private readonly stdin: Writable
-  private readonly stdout: AsyncGenerator<string>
-  private readonly stderr: AsyncGenerator<string>
-
-  private constructor(
-    dart: ChildProcessWithoutNullStreams,
-    private readonly initArgs: string[] = []
-  ) {
-    this.stdin = dart.stdin
-    this.stdout = toDartChunks(dart.stdout)
-    this.stderr = toDartChunks(dart.stderr)
-    this.initArgs = initArgs
+  // Split a buffer using the given token
+  private static split(buffer: Buffer, token: number) {
+    const segments = []
+    let [head, tail] = this.splitSingle(buffer, token)
+    while (tail) {
+      segments.push(head)
+      const [head2, tail2] = this.splitSingle(tail, token)
+      head = head2
+      tail = tail2
+    }
+    segments.push(head)
+    return segments
   }
 
-  /**
-   * Create a dart-sass compiler from the repo given by the path.
-   */
-  static async fromRepo(path: string, initArgs: string[] = []) {
-    return new DartCompiler(await createDartProcess(path), initArgs)
-  }
-
-  async compile(path: string, opts: string[]) {
-    this.stdin.write(`!cd ${path}\n`)
-    this.stdin.write([...this.initArgs, ...opts].join(" ") + "\n")
-    return {
-      stdout: (await this.stdout.next()).value,
-      stderr: (await this.stderr.next()).value,
-      status: +(await this.stdout.next()).value,
+  // Split the stream into chunks based on the break character (0xff)
+  // TODO need to test this
+  private static async *toChunks(stream: Readable) {
+    let buff = Buffer.from("")
+    for await (const chunk of stream) {
+      const [head, ...tail] = this.split(chunk, 0xff)
+      // If we received *any* break characters, yield those segments
+      if (tail.length > 0) {
+        yield Buffer.concat([buff, head]).toString()
+        for (const item of tail.slice(0, tail.length - 1)) {
+          yield item.toString()
+        }
+        // Set the buffer to the last unfinished segment
+        buff = tail[tail.length - 1]
+      } else {
+        // If we didn't receive any 0xff in this chunk, just append to the
+        // intermediate buffer
+        buff = Buffer.concat([buff, head])
+      }
     }
   }
 }
