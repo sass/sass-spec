7244865c0dd2c89be69b747860d92698ddf8cf5a
Natalie Weizenbaum <nweiz@google.com>
2021-10-19 21:34:13 -0700

    Work around facebook/jest#2549

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,8 @@ import type {Config} from '@jest/types';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  // Work around facebook/jest#2549
+  testEnvironment: 'jest-environment-node-single-context',
   roots: ['test'],
   setupFilesAfterEnv: ['jest-extended'],
 };
