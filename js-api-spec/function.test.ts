6c5f2fef33fb04ff283306d375edb7b4110e9f64
Natalie Weizenbaum <nweiz@google.com>
2021-12-20 16:11:50 -0800

    Enable most custom function specs for the embedded host

--- js-api-spec/function.test.ts
+++ js-api-spec/function.test.ts
@@ -14,72 +14,72 @@ import {
 
 import {skipForImpl} from './utils';
 
+it('passes an argument to a custom function and uses its return value', () => {
+  const fn = jest.fn(args => {
+    expect(args).toHaveLength(1);
+    expect(args[0].assertString().text).toBe('bar');
+    return new SassString('result');
+  });
+
+  expect(
+    compileString('a {b: foo(bar)}', {
+      functions: {'foo($arg)': fn},
+    }).css
+  ).toBe('a {\n  b: "result";\n}');
+
+  expect(fn).toBeCalled();
+});
+
+it('passes no arguments to a custom function', () => {
+  const fn = jest.fn(args => {
+    expect(args).toHaveLength(0);
+    return sassNull;
+  });
+
+  expect(
+    compileString('a {b: foo()}', {
+      functions: {'foo()': fn},
+    }).css
+  ).toBe('');
+
+  expect(fn).toBeCalled();
+});
+
+it('passes multiple arguments to a custom function', () => {
+  const fn = jest.fn(args => {
+    expect(args).toHaveLength(3);
+    expect(args[0].assertString().text).toBe('x');
+    expect(args[1].assertString().text).toBe('y');
+    expect(args[2].assertString().text).toBe('z');
+    return sassNull;
+  });
+
+  expect(
+    compileString('a {b: foo(x, y, z)}', {
+      functions: {'foo($arg1, $arg2, $arg3)': fn},
+    }).css
+  ).toBe('');
+
+  expect(fn).toBeCalled();
+});
+
+it('passes a default argument value', () => {
+  const fn = jest.fn(args => {
+    expect(args).toHaveLength(1);
+    expect(args[0].assertString().text).toBe('default');
+    return sassNull;
+  });
+
+  expect(
+    compileString('a {b: foo()}', {
+      functions: {'foo($arg: default)': fn},
+    }).css
+  ).toBe('');
+
+  expect(fn).toBeCalled();
+});
+
 skipForImpl('sass-embedded', () => {
-  it('passes an argument to a custom function and uses its return value', () => {
-    const fn = jest.fn(args => {
-      expect(args).toHaveLength(1);
-      expect(args[0].assertString().text).toBe('bar');
-      return new SassString('result');
-    });
-
-    expect(
-      compileString('a {b: foo(bar)}', {
-        functions: {'foo($arg)': fn},
-      }).css
-    ).toBe('a {\n  b: "result";\n}');
-
-    expect(fn).toBeCalled();
-  });
-
-  it('passes no arguments to a custom function', () => {
-    const fn = jest.fn(args => {
-      expect(args).toHaveLength(0);
-      return sassNull;
-    });
-
-    expect(
-      compileString('a {b: foo()}', {
-        functions: {'foo()': fn},
-      }).css
-    ).toBe('');
-
-    expect(fn).toBeCalled();
-  });
-
-  it('passes multiple arguments to a custom function', () => {
-    const fn = jest.fn(args => {
-      expect(args).toHaveLength(3);
-      expect(args[0].assertString().text).toBe('x');
-      expect(args[1].assertString().text).toBe('y');
-      expect(args[2].assertString().text).toBe('z');
-      return sassNull;
-    });
-
-    expect(
-      compileString('a {b: foo(x, y, z)}', {
-        functions: {'foo($arg1, $arg2, $arg3)': fn},
-      }).css
-    ).toBe('');
-
-    expect(fn).toBeCalled();
-  });
-
-  it('passes a default argument value', () => {
-    const fn = jest.fn(args => {
-      expect(args).toHaveLength(1);
-      expect(args[0].assertString().text).toBe('default');
-      return sassNull;
-    });
-
-    expect(
-      compileString('a {b: foo()}', {
-        functions: {'foo($arg: default)': fn},
-      }).css
-    ).toBe('');
-
-    expect(fn).toBeCalled();
-  });
-
   it('passes an argument list', () => {
     const fn = jest.fn(args => {
       expect(args).toHaveLength(1);
@@ -119,126 +119,126 @@ skipForImpl('sass-embedded', () => {
 
     expect(fn).toBeCalled();
   });
+});
 
-  describe('gracefully handles a custom function', () => {
-    it('throwing', () => {
-      expect(() =>
-        compileString('a {b: foo()}', {
-          functions: {
-            'foo()': () => {
-              throw 'heck';
-            },
+describe('gracefully handles a custom function', () => {
+  it('throwing', () => {
+    expect(() =>
+      compileString('a {b: foo()}', {
+        functions: {
+          'foo()': () => {
+            throw 'heck';
           },
-        })
-      ).toThrowSassException({line: 0});
-    });
-
-    it('not returning', () => {
-      expect(() =>
-        compileString('a {b: foo()}', {
-          functions: {'foo()': (() => {}) as unknown as CustomFunction<'sync'>},
-        })
-      ).toThrowSassException({line: 0});
-    });
-
-    it('returning a non-Value', () => {
-      expect(() =>
-        compileString('a {b: foo()}', {
-          functions: {
-            'foo()': (() => 'wrong') as unknown as CustomFunction<'sync'>,
-          },
-        })
-      ).toThrowSassException({line: 0});
-    });
+        },
+      })
+    ).toThrowSassException({line: 0});
   });
 
-  describe('dash-normalizes function calls', () => {
-    it('when defined with dashes', () => {
-      const fn = jest.fn(() => sassNull);
-
-      expect(
-        compileString('a {b: foo_bar()}', {
-          functions: {'foo-bar()': fn},
-        }).css
-      ).toBe('');
-
-      expect(fn).toBeCalled();
-    });
-
-    it('when defined with underscores', () => {
-      const fn = jest.fn(() => sassNull);
-
-      expect(
-        compileString('a {b: foo-bar()}', {
-          functions: {'foo_bar()': fn},
-        }).css
-      ).toBe('');
-
-      expect(fn).toBeCalled();
-    });
+  it('not returning', () => {
+    expect(() =>
+      compileString('a {b: foo()}', {
+        functions: {'foo()': (() => {}) as unknown as CustomFunction<'sync'>},
+      })
+    ).toThrowSassException({line: 0});
   });
 
-  describe('asynchronously', () => {
-    it('passes an argument to a custom function and uses its return value', async () => {
-      const fn = jest.fn(args => {
-        expect(args).toHaveLength(1);
-        expect(args[0].assertString().text).toBe('bar');
-        return Promise.resolve(new SassString('result'));
-      });
+  it('returning a non-Value', () => {
+    expect(() =>
+      compileString('a {b: foo()}', {
+        functions: {
+          'foo()': (() => 'wrong') as unknown as CustomFunction<'sync'>,
+        },
+      })
+    ).toThrowSassException({line: 0});
+  });
+});
 
-      await expect(
-        compileStringAsync('a {b: foo(bar)}', {
-          functions: {'foo($arg)': fn},
-        })
-      ).resolves.toMatchObject({css: 'a {\n  b: "result";\n}'});
+describe('dash-normalizes function calls', () => {
+  it('when defined with dashes', () => {
+    const fn = jest.fn(() => sassNull);
 
-      expect(fn).toBeCalled();
-    });
+    expect(
+      compileString('a {b: foo_bar()}', {
+        functions: {'foo-bar()': fn},
+      }).css
+    ).toBe('');
 
-    it('gracefully handles promise rejections', async () => {
-      await expect(() =>
-        compileStringAsync('a {b: foo(bar)}', {
-          functions: {'foo($arg)': () => Promise.reject('heck')},
-        })
-      ).toThrowSassException({line: 0});
-    });
+    expect(fn).toBeCalled();
   });
 
-  describe('rejects a function signature that', () => {
-    it('is empty', () => {
-      expect(() =>
-        compileString('', {functions: {'': () => sassNull}})
-      ).toThrow();
+  it('when defined with underscores', () => {
+    const fn = jest.fn(() => sassNull);
+
+    expect(
+      compileString('a {b: foo-bar()}', {
+        functions: {'foo_bar()': fn},
+      }).css
+    ).toBe('');
+
+    expect(fn).toBeCalled();
+  });
+});
+
+describe('asynchronously', () => {
+  it('passes an argument to a custom function and uses its return value', async () => {
+    const fn = jest.fn(args => {
+      expect(args).toHaveLength(1);
+      expect(args[0].assertString().text).toBe('bar');
+      return Promise.resolve(new SassString('result'));
     });
 
-    it('has no name', () => {
-      expect(() =>
-        compileString('', {functions: {'()': () => sassNull}})
-      ).toThrow();
-    });
+    await expect(
+      compileStringAsync('a {b: foo(bar)}', {
+        functions: {'foo($arg)': fn},
+      })
+    ).resolves.toMatchObject({css: 'a {\n  b: "result";\n}'});
 
-    it('has no arguments', () => {
-      expect(() =>
-        compileString('', {functions: {foo: () => sassNull}})
-      ).toThrow();
-    });
+    expect(fn).toBeCalled();
+  });
 
-    it('has invalid arguments', () => {
-      expect(() =>
-        compileString('', {functions: {'foo(arg)': () => sassNull}})
-      ).toThrow();
-    });
+  it('gracefully handles promise rejections', async () => {
+    await expect(() =>
+      compileStringAsync('a {b: foo(bar)}', {
+        functions: {'foo($arg)': () => Promise.reject('heck')},
+      })
+    ).toThrowSassException({line: 0});
+  });
+});
 
-    it('has no closing parentheses', () => {
-      expect(() =>
-        compileString('', {functions: {'foo(': () => sassNull}})
-      ).toThrow();
-    });
+describe('rejects a function signature that', () => {
+  it('is empty', () => {
+    expect(() =>
+      compileString('', {functions: {'': () => sassNull}})
+    ).toThrow();
+  });
 
-    it('has a non-identifier name', () => {
-      expect(() =>
-        compileString('', {functions: {'$foo()': () => sassNull}})
-      ).toThrow();
-    });
+  it('has no name', () => {
+    expect(() =>
+      compileString('', {functions: {'()': () => sassNull}})
+    ).toThrow();
+  });
+
+  it('has no arguments', () => {
+    expect(() =>
+      compileString('', {functions: {foo: () => sassNull}})
+    ).toThrow();
+  });
+
+  it('has invalid arguments', () => {
+    expect(() =>
+      compileString('', {functions: {'foo(arg)': () => sassNull}})
+    ).toThrow();
+  });
+
+  it('has no closing parentheses', () => {
+    expect(() =>
+      compileString('', {functions: {'foo(': () => sassNull}})
+    ).toThrow();
+  });
+
+  it('has a non-identifier name', () => {
+    expect(() =>
+      compileString('', {functions: {'$foo()': () => sassNull}})
+    ).toThrow();
   });
 });
