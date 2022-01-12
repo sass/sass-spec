5cac699b7039844a95aa8c3c84c2eafd7a199621
Natalie Weizenbaum <nweiz@google.com>
2022-01-11 17:35:36 -0800

    Enable null and boolean specs for the embedded host

--- js-api-spec/legacy/value/boolean.test.ts
+++ js-api-spec/legacy/value/boolean.test.ts
@@ -4,47 +4,43 @@
 
 import * as sass from 'sass';
 
-import {skipForImpl} from '../../utils';
 import {parseValue} from './utils';
 
-skipForImpl('sass-embedded', () => {
-  describe('from a parameter', () => {
-    it('true is true', () =>
-      expect(parseValue('true', sass.types.Boolean).getValue()).toBeTrue());
+describe('from a parameter', () => {
+  it('true is true', () =>
+    expect(parseValue('true', sass.types.Boolean).getValue()).toBeTrue());
 
-    it('false is false', () =>
-      expect(parseValue('false', sass.types.Boolean).getValue()).toBeFalse());
+  it('false is false', () =>
+    expect(parseValue('false', sass.types.Boolean).getValue()).toBeFalse());
+});
+
+describe('from a constant', () => {
+  it('true is true', () => {
+    const value = sass.types.Boolean.TRUE;
+    expect(value).toBeInstanceOf(sass.types.Boolean);
+    expect(value.getValue()).toBeTrue();
   });
 
-  describe('from a constant', () => {
-    it('true is true', () => {
-      const value = sass.types.Boolean.TRUE;
-      expect(value).toBeInstanceOf(sass.types.Boolean);
-      expect(value.getValue()).toBeTrue();
-    });
-
-    it('false is false', () => {
-      const value = sass.types.Boolean.FALSE;
-      expect(value).toBeInstanceOf(sass.types.Boolean);
-      expect(value.getValue()).toBeFalse();
-    });
+  it('false is false', () => {
+    const value = sass.types.Boolean.FALSE;
+    expect(value).toBeInstanceOf(sass.types.Boolean);
+    expect(value.getValue()).toBeFalse();
   });
+});
 
-  describe('the constructor throws', () => {
-    it('with a parameter', () =>
-      expect(
-        () => new (sass.types.Boolean as new (value: boolean) => unknown)(true)
-      ).toThrow());
+describe('the constructor throws', () => {
+  it('with a parameter', () =>
+    expect(
+      () => new (sass.types.Boolean as new (value: boolean) => unknown)(true)
+    ).toThrow());
 
-    it('with no parameters', () =>
-      expect(() => new (sass.types.Boolean as new () => unknown)()).toThrow());
-  });
+  it('with no parameters', () =>
+    expect(() => new (sass.types.Boolean as new () => unknown)()).toThrow());
+});
 
-  describe('the convenience accessor', () => {
-    it('sass.TRUE exists', () =>
-      expect(sass.TRUE).toBe(sass.types.Boolean.TRUE));
+describe('the convenience accessor', () => {
+  it('sass.TRUE exists', () => expect(sass.TRUE).toBe(sass.types.Boolean.TRUE));
 
-    it('sass.FALSE exists', () =>
-      expect(sass.FALSE).toBe(sass.types.Boolean.FALSE));
-  });
+  it('sass.FALSE exists', () =>
+    expect(sass.FALSE).toBe(sass.types.Boolean.FALSE));
 });
