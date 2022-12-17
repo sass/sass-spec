ac80a67b617b5d0936cc46484ee83aaf3c7df91d
Natalie Weizenbaum <nweiz@google.com>
2022-12-16 16:16:25 -0800

    Rename `fromPath()` to `fromRoot()` (#1858)
    This clarifies that this should only be used for the root directory of
    a test suite, since it won't consider any options that come from
    parent directories. This also refactors lint-spec.ts to avoid using
    this function for subdirectories.

--- lib/spec-directory/index.ts
+++ lib/spec-directory/index.ts
@@ -5,13 +5,13 @@ import {resolveSpecPath} from './spec-path';
 
 /**
  * Creates either a physical SpecDirectory from a directory or a virtual one
- * from an hrx archive.
+ * from an hrx archive representing the root of a test suite.
  *
  * The `path` parameter may end in `/`, `.hrx`, both, or neither, and will be
  * able to load both HRX archives and physical directories regardless of which
  * suffix it uses.
  */
-export async function fromPath(path: string): Promise<SpecDirectory> {
+export async function fromRoot(path: string): Promise<SpecDirectory> {
   const resolved = resolveSpecPath(path);
   return resolved.endsWith('.hrx')
     ? await VirtualDirectory.fromArchive(resolved)
