82bfdd68f711faa009aa287084ba4cec539aaa06
Jonny Gerig Meyer <jonny@oddbird.net>
2023-04-21 13:09:15 -0400

    add copyright

--- js-api-spec/browser/utils.ts
+++ js-api-spec/browser/utils.ts
@@ -1,3 +1,7 @@
+// Copyright 2021 Google Inc. Use of this source code is governed by an
+// MIT-style license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 /* Whether the tests are running in a browser context. */
 export const isBrowser = !global.process;
 
