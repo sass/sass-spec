f9188471e3be73de1bab4a8ea88d0bd211188ff4
Connor Skees <connor1skees@gmail.com>
2023-09-29 19:04:28 +0000

    add js api mixin test

---
+++ js-api-spec/value/mixin.test.ts
@@ -0,0 +1,32 @@
+// Copyright 2021 Google Inc. Use of this source code is governed by an
+// MIT-style license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import {SassMixin, compileString} from 'sass';
+
+import {spy} from '../utils';
+
+it('can round-trip a mixin reference from Sass', () => {
+  const fn = spy(args => {
+    expect(args).toBeArrayOfSize(1);
+    expect(args[0]).toBeInstanceOf(SassMixin);
+    args[0].assertMixin();
+    return args[0];
+  });
+
+  expect(
+    compileString(
+      `
+      @use 'sass:meta';
+
+      @mixin a() {}
+      a {b: meta.inspect(foo(meta.get-mixin('a')))}
+    `,
+      {
+        functions: {'foo($arg)': fn},
+      }
+    ).css
+  ).toBe('a {\n  b: get-mixin("a");\n}');
+
+  expect(fn).toHaveBeenCalled();
+});
